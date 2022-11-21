2908fbbc2e3cf194ff868eafa6c8e9c067a4373b
Tolmachev DY <Tolmachev DY@DESKTOP-5RJHKAS>
2022-11-21 15:03:31 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите данные");
+            Console.WriteLine("Градусы:");
+            int gradus_ = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Минуты:");
+            int min_ = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Секунды:");
+            int sec_ = Convert.ToInt32(Console.ReadLine());
+
+            Radians radians = new Radians(gradus_, min_, sec_);
+
+            float totalRadians = radians.ToRadians();
+            Console.WriteLine("Радианы = {0:f4}", totalRadians);
+            Console.ReadKey();
+        }
+
+    }
+}
+
---
+++ Task1/Radians.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Radians
+    {
+        int gradus;
+        int min;
+        int sec;
+
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+
+            set
+            {
+                gradus = value;
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                Gradus += value / 60;
+                min = value % 60;
+            }
+        }
+        public int Sec
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                Min += value / 60;
+                sec = value % 60;
+            }
+        }
+
+        public Radians(int gradus, int min, int sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+
+        public float ToRadians()
+        {
+            return (float)((gradus + (min + sec / 60.0) / 60) * Math.PI / 180);
+        }
+    }
+}
